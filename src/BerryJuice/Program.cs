a56421537340c46b74b4f572bc31277a25806466
Shirasagi <23011330+Shirasagi0012@users.noreply.github.com>
2024-07-16 22:47:47 +0800

    init: Created three projects

---
+++ src/BerryJuice/Program.cs
@@ -0,0 +1,28 @@
+using BerryJuice.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if ( app.Environment.IsDevelopment() )
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BerryJuice.Client._Imports).Assembly);
+
+app.Run();
