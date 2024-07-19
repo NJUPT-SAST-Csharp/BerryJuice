7ffff0ef053929a96a72a62872d95b348f8a803c
Shirasagi <23011330+Shirasagi0012@users.noreply.github.com>
2024-07-20 03:23:58 +0800

    restart: Project reset due to problems caused by renaming project

--- src/BerryJuice/Program.cs
+++ src/BerryJuice/Program.cs
@@ -1,28 +1,49 @@
-using BerryJuice.Components;
+using BerryJuice.Blazor.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveWebAssemblyComponents();
+if (Environment.GetEnvironmentVariable("USE_BLAZOR") == "true")
+    builder
+        .Services.AddRazorComponents()
+        .AddInteractiveServerComponents()
+        .AddInteractiveWebAssemblyComponents();
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if ( app.Environment.IsDevelopment() )
+if (app.Environment.IsDevelopment())
 {
-    app.UseWebAssemblyDebugging();
+    if (Environment.GetEnvironmentVariable("USE_BLAZOR") == "true")
+        app.UseWebAssemblyDebugging();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 else
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    if (Environment.GetEnvironmentVariable("USE_BLAZOR") == "true")
+        app.UseExceptionHandler("/Error", createScopeForErrors: true);
 }
 
+app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(BerryJuice.Client._Imports).Assembly);
+if (Environment.GetEnvironmentVariable("USE_BLAZOR") == "true")
+{
+    app.MapControllers();
+    app.MapRazorComponents<App>()
+        .AddInteractiveWebAssemblyRenderMode()
+        .AddAdditionalAssemblies(typeof(BerryJuice.Blazor.Client._Imports).Assembly);
+}
 
 app.Run();
