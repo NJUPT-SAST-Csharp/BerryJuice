874e2053f5f7f1d867749db47635247a32922277
Lint Action <lint-action@samuelmeuli.com>
2024-08-09 12:44:25 +0000

    Fix code style issues with dotnet_format

--- src/Backend/BerryJuice.Infrastructure/Persistence/BerryJuiceDbContext.cs
+++ src/Backend/BerryJuice.Infrastructure/Persistence/BerryJuiceDbContext.cs
@@ -3,4 +3,5 @@
 namespace BerryJuice.Infrastructure.Persistence;
 
 public class BerryJuiceDbContext(DbContextOptions<BerryJuiceDbContext> options)
-    : DbContext(options) { }
+    : DbContext(options)
+{ }
