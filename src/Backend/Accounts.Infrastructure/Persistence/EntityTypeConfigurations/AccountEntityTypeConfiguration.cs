c2cc018160a28af0fc037523865b6f0384b29eb1
Shirasagi <23011330+Shirasagi0012@users.noreply.github.com>
2024-08-15 18:03:13 +0800

    feat: accountsContext

---
+++ src/Backend/Accounts.Infrastructure/Persistence/EntityTypeConfigurations/AccountEntityTypeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Accounts.Domain.AccountAggregate.AccountEntity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Accounts.Infrastructure.Persistence.EntityTypeConfigurations;
+
+internal class AccountEntityTypeConfiguration : IEntityTypeConfiguration<Account>
+{
+    public void Configure(EntityTypeBuilder<Account> builder)
+    {
+        throw new NotImplementedException();
+    }
+}
