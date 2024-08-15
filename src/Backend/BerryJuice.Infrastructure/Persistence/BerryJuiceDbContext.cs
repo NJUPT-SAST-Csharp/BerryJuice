296e98d16220e6e39b40e45f3e137d5a13ba07f2
Shirasagi <23011330+Shirasagi0012@users.noreply.github.com>
2024-08-15 17:59:40 +0800

    feat: main dbcontext

--- src/Backend/BerryJuice.Infrastructure/Persistence/BerryJuiceDbContext.cs
+++ src/Backend/BerryJuice.Infrastructure/Persistence/BerryJuiceDbContext.cs
@@ -2,6 +2,10 @@
 
 namespace BerryJuice.Infrastructure.Persistence;
 
-public class BerryJuiceDbContext(DbContextOptions<BerryJuiceDbContext> options)
-    : DbContext(options)
-{ }
+public class BerryJuiceDbContext(DbContextOptions<BerryJuiceDbContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(BerryJuiceDbContext).Assembly);
+    }
+}
