6bbd1f6a968d7ac1e4483a7e991462db1608ce4b
Shirasagi <23011330+Shirasagi0012@users.noreply.github.com>
2024-08-15 13:33:12 +0800

    test: Add example query

---
+++ src/Backend/Accounts.Application/TestService/GetTest/TestQuery.cs
@@ -0,0 +1,8 @@
+﻿using Shared.Primitives.Query;
+
+namespace Accounts.Application.TestService.GetTest;
+
+public sealed class TestQuery() : IQueryRequest<int>
+{
+    public int Number { get; } = Random.Shared.Next();
+}
---
+++ src/Backend/Accounts.Application/TestService/GetTest/TestQueryHandler.cs
@@ -0,0 +1,11 @@
+﻿using Shared.Primitives.Query;
+
+namespace Accounts.Application.TestService.GetTest;
+
+internal sealed class TestQueryHandler : IQueryRequestHandler<TestQuery, int>
+{
+    public Task<int> Handle(TestQuery request, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(request.Number);
+    }
+}
