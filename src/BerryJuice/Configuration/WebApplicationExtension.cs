030581fc944bf029d3f89e8ba069b596163aa3ab
Shirasagi <23011330+Shirasagi0012@users.noreply.github.com>
2024-08-27 02:42:32 +0800

    feat: Create and apply migrations

--- src/BerryJuice/Configuration/WebApplicationExtension.cs
+++ src/BerryJuice/Configuration/WebApplicationExtension.cs
@@ -1,15 +1,17 @@
-﻿using BerryJuice.Blazor.Components;
+﻿using Accounts.Infrastructure.Persistence;
+using BerryJuice.Blazor.Components;
+using Microsoft.EntityFrameworkCore;
 using _Imports = BerryJuice.Blazor.Client._Imports;
 
 namespace BerryJuice.Configuration;
 
 public static class WebApplicationExtension
 {
-    public static WebApplication ConfigureApplication(this WebApplication app)
+    public static WebApplication ConfigureApplication (this WebApplication app)
     {
         var isBlazorEnabled = app.Configuration["BERRYJUICE_USE_BLAZOR"] == "true";
 
-        if (app.Environment.IsDevelopment())
+        if ( app.Environment.IsDevelopment() )
         {
             app.UseSwagger();
             app.UseSwaggerUI();
@@ -21,9 +23,9 @@ public static class WebApplicationExtension
         app.UseAuthorization();
         app.MapControllers();
 
-        if (isBlazorEnabled)
+        if ( isBlazorEnabled )
         {
-            if (app.Environment.IsDevelopment())
+            if ( app.Environment.IsDevelopment() )
                 app.UseWebAssemblyDebugging();
             else
                 app.UseExceptionHandler("/Error", true);
@@ -34,6 +36,11 @@ public static class WebApplicationExtension
                .AddInteractiveServerRenderMode()
                .AddAdditionalAssemblies(typeof(_Imports).Assembly);
         }
+
+        // apply migrations
+        using var scope = app.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<AccountsContext>();
+        context.Database.Migrate();
 
         return app;
     }
