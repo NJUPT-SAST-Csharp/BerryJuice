a31ebfda95a027d6d77aabb94fecbe42806e061e
Shirasagi <23011330+Shirasagi0012@users.noreply.github.com>
2024-08-27 02:42:32 +0800

    feat: Create and apply migrations

---
+++ src/Backend/Accounts.Infrastructure/Persistence/Migrations/20240826183722_AccountsInitial.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Accounts.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AccountsInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "BJAccounts");
+
+            migrationBuilder.CreateTable(
+                name: "accounts",
+                schema: "BJAccounts",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_accounts", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tags",
+                schema: "BJAccounts",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_tags", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "transactions",
+                schema: "BJAccounts",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false),
+                    account_id = table.Column<long>(type: "bigint", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    currency = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_transactions", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_transactions_accounts_account_id",
+                        column: x => x.account_id,
+                        principalSchema: "BJAccounts",
+                        principalTable: "accounts",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "transactions_tags",
+                schema: "BJAccounts",
+                columns: table => new
+                {
+                    transaction_id = table.Column<long>(type: "bigint", nullable: false),
+                    tag_id = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_transactions_tags", x => new { x.transaction_id, x.tag_id });
+                    table.ForeignKey(
+                        name: "fk_transactions_tags_tags_tag_id",
+                        column: x => x.tag_id,
+                        principalSchema: "BJAccounts",
+                        principalTable: "tags",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_transactions_tags_transactions_transaction_id",
+                        column: x => x.transaction_id,
+                        principalSchema: "BJAccounts",
+                        principalTable: "transactions",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_transactions_account_id",
+                schema: "BJAccounts",
+                table: "transactions",
+                column: "account_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_transactions_tags_tag_id",
+                schema: "BJAccounts",
+                table: "transactions_tags",
+                column: "tag_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "transactions_tags",
+                schema: "BJAccounts");
+
+            migrationBuilder.DropTable(
+                name: "tags",
+                schema: "BJAccounts");
+
+            migrationBuilder.DropTable(
+                name: "transactions",
+                schema: "BJAccounts");
+
+            migrationBuilder.DropTable(
+                name: "accounts",
+                schema: "BJAccounts");
+        }
+    }
+}
