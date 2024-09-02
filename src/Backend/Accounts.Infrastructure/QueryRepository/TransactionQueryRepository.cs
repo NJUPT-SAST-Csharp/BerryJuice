7d8c074355d52301bb56d0173803319ed4ecd52e
Shirasagi <23011330+Shirasagi0012@users.noreply.github.com>
2024-09-02 22:02:49 +0800

    fix(CQRS): now can return entries with no tags

--- src/Backend/Accounts.Infrastructure/QueryRepository/TransactionQueryRepository.cs
+++ src/Backend/Accounts.Infrastructure/QueryRepository/TransactionQueryRepository.cs
@@ -34,20 +34,21 @@ internal class TransactionQueryRepository(IDbConnectionFactory factory) : IGetTr
 
         const string sql2 =
             @"
-                SELECT  t.id as Id, 
+                SELECT  t.id as TransactionId, 
                         t.amount as Amount, 
                         t.currency as CurrencyType, 
                         t.description as Description, 
-                        t.created_at as TimeCreated,
-                        tags.id as TagId,
-                        tags.name as TagName
+                        t.created_at as TimeCreated
                 FROM bj_accounts.transactions t
-                INNER JOIN bj_accounts.transactions_tags tt ON tt.transaction_id = t.id
-                INNER JOIN bj_accounts.tags tags ON tags.id = tt.tag_id
                 WHERE t.account_id = @AccountId
             ";
 
-        var transactions = await _connection.QueryAsync<TransactionDto, TagDto, TransactionDto>(
+        var transactions = await _connection.QueryAsync<TransactionDto>(
+            sql2,
+            new { AccountId = id.Value }
+        );
+
+        var transactions_tag = await _connection.QueryAsync<TransactionDto, TagDto, TransactionDto>(
             sql1,
             (transaction, tag) =>
             {
@@ -58,18 +59,26 @@ internal class TransactionQueryRepository(IDbConnectionFactory factory) : IGetTr
             splitOn: "TagId"
         );
 
-        var result = transactions
-            .GroupBy(t => t.TransactionId)
-            .Select(g =>
-            {
-                var transaction = g.First() with
-                {
-                    Tags = g.Select(t => new TagDto(t.Tags.First().TagId, t.Tags.First().TagName))
-                        .ToList()
-                };
-                return transaction;
-            });
+        foreach (var tagEntry in transactions_tag)
+        {
+            var transaction = transactions.FirstOrDefault(t =>
+                t.TransactionId == tagEntry.TransactionId
+            );
+            transaction?.Tags.Add(tagEntry.Tags.First());
+        }
 
-        return result;
+        //var result = transactions_tag
+        //    .GroupBy(t => t.TransactionId)
+        //    .Select(g =>
+        //    {
+        //        var transaction = g.First() with
+        //        {
+        //            Tags = g.Select(t => new TagDto(t.Tags.First().TagId, t.Tags.First().TagName))
+        //                .ToList()
+        //        };
+        //        return transaction;
+        //    });
+
+        return transactions;
     }
 }
