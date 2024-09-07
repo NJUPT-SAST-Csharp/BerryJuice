8e206d44f9f48a8cfa079abb0ffe8f7ce5925f43
Shirasagi <23011330+Shirasagi0012@users.noreply.github.com>
2024-09-07 21:46:43 +0800

    feat: Wallet Page

---
+++ src/Frontend/BerryJuice.Blazor/Components/Pages/Transactions/TransactionList.razor.cs
@@ -0,0 +1,32 @@
+﻿using Accounts.Application.TransactionService.Models;
+using Accounts.Application.TransactionService.Queries;
+using Microsoft.AspNetCore.Components;
+using Enums;
+
+namespace BerryJuice.Blazor.Components.Pages.Transactions;
+
+public partial class TransactionList : ComponentBase
+{
+    [CascadingParameter(Name = "AccountId")]
+    public long Id
+    {
+        get => _id;
+        set
+        {
+            if (value != _id && value > 0)
+            {
+                _ = UpdateTransactionListAsync(value);
+                _id = value;
+            }
+        }
+    }
+
+    private async Task UpdateTransactionListAsync(long id)
+    {
+        _transactions = (await EventBus.QueryAsync(new GetTransactionsQuery(id))).Transactions.ToList();
+        StateHasChanged();
+    }
+
+    private List<TransactionModel> _transactions = [];
+    private long _id;
+}
---
+++ src/Frontend/BerryJuice.Blazor/Components/Pages/Transactions/Wallet.razor.cs
@@ -0,0 +1,36 @@
+﻿using Accounts.Application.AccountService.Models;
+using Accounts.Application.AccountService.Queries;
+using Accounts.Application.TransactionService.Commands;
+using Accounts.Application.TransactionService.Queries;
+using Enums;
+
+namespace BerryJuice.Blazor.Components.Pages.Transactions;
+
+public partial class Wallet
+{
+    public long SelectedAccountId { get; set; } = 0;
+
+    public string? MoneyInput { get; set; }
+    private List<AccountModel> _accounts = [];
+
+    protected override async Task OnInitializedAsync()
+    {
+        await UpdateAccountListAsync();
+    }
+
+    private async Task UpdateAccountListAsync()
+    {
+        _accounts = (await EventBus.QueryAsync(new GetAccountsQuery())).Accounts.ToList();
+        StateHasChanged();
+    }
+
+    private async Task AddTransactionAsync()
+    {
+        if (SelectedAccountId != 0 && Decimal.TryParse(MoneyInput, out var moneyValue))
+        {
+            var transactionId = await EventBus.CommandAsync(
+                new AddTransactionCommand(SelectedAccountId, CurrencyType.CNY, moneyValue, [], description: "test")
+            );
+        }
+    }
+}
